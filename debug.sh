becdd72487f23e42ce2f6d1117c8a43e48077580
Berkay Guler <you@example.com>
2023-05-06 19:22:00 +0300

    added debug.sh as testing script edited .gitignore refactored simpler_online_training.py

---
+++ debug.sh
@@ -0,0 +1,5 @@
+python3 simpler_online_training.py  --replay_sample_nb 100\
+                                    --device 0 \
+                                    --epochs 5 \
+                                    --threshold 10 \
+                                    --batch_size 8
