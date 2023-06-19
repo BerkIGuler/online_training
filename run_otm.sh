c6a49a4bbf4f2184e18c9dbed2cee539835ba105
Berkay Guler <you@example.com>
2023-06-19 22:39:31 +0300

    renamed debug_otm.sh to run_otm.sh

--- run_otm.sh
+++ run_otm.sh
@@ -2,7 +2,7 @@ python3 simpler_online_training.py  -i "127.0.0.1" \
                                     -p 65432 \
                                     --replay_sample_nb 10 \
                                     --device 0 \
-                                    --epochs 5 \
-                                    --threshold 3 \
+                                    --epochs 1000 \
+                                    --threshold 100 \
                                     --batch_size 4 \
                                     --patience 2
